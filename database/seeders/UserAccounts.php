13b68569ad462b6c86cd9ffc29c3810c115b1978
KevinCasoco <andreikevincasoco@gmail.com>
2024-01-30 14:47:36 +0800

    initial commit

---
+++ database/seeders/UserAccounts.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+
+class UserAccounts extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create(); // Create a Faker instance
+
+        // seeding admin
+        $admin = User::create([
+            'name' => $faker->name(),
+            'email' => 'admin@sample.com',
+            'password' => Hash::make('12345'),
+            'role' => 'admin',
+        ]);
+
+        // seeding editor
+        $editor = User::create([
+            'name' => $faker->name(),
+            'email' => 'editor@sample.com',
+            'password' => Hash::make('12345'),
+            'role' => 'editor',
+        ]);
+
+        // seeding user
+        $editor = User::create([
+            'name' => $faker->name(),
+            'email' => 'user@sample.com',
+            'password' => Hash::make('12345'),
+            'role' => 'user',
+        ]);
+    }
+}
