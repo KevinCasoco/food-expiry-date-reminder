b96ea41d2f14255ba6e1a3c7c944ef54ad5c368b
KevinCasoco <andreikevincasoco@gmail.com>
2024-05-08 02:44:54 +0800

    admin view products modal fixed!

--- resources/views/admin/admin-product-information.blade.php
+++ resources/views/admin/admin-product-information.blade.php
@@ -169,7 +169,7 @@
                                 <td >{{ $products->id }}</td>
                                 {{-- bar code with id --}}
                                 {{-- <td class="h-16">
-                                    {!! DNS1D::getBarcodeHTML("$products->product_code", 'C128', 2, 60) !!} 
+                                    {!! DNS1D::getBarcodeHTML("$products->product_code", 'C128', 2, 60) !!}
                                     <p>p - {{ $products->product_code }}</p>
                                 </td> --}}
                                 {{-- qr code --}}
@@ -197,8 +197,7 @@
                                     </button>
                                 </td>
                                 <td class="text-center ">
-                                    <button
-                                        @click="adminView = true; itemToView = $event.target.getAttribute('data-item-id')"
+                                    <button @click="adminView = true; itemToView = $event.target.getAttribute('data-item-id')"
                                         data-item-id="{{ $products->id }}"
                                         class="py-1 px-4 rounded bg-[#4ECE5D] hover:bg-[#4ECE5D] text-white">
                                         <i class="ri-edit-box-fill mr-1"></i>View
@@ -308,31 +307,33 @@
                         </div>
                         <hr class="bg-black border-gray-300 w-full">
                         @foreach ($data as $products)
-                            <div x-show="itemToView.toString() === '{{ $products->id }}'">
+                            <div x-show="itemToView === '{{ $products->id }}'">
                                 <form method="post"
                                     :action="`{{ route('user.product-information.update_products', '') }}/${itemToEdit}`"
                                     class="pl-5 pr-5 pt-2 pb-1">
                                     @csrf
                                     @method('patch')
-                                
-                                    {{-- <label for="barcode_id"
-                                        class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Barcode ID:</label>
-                                    <input type="number" name="id" value="{{ $products->product_code }}"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mb-2 p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white  w-full sm:w-[300px]"
-                                        disabled> --}}
 
-                                        <div class="flex flex-col items-center">
+                                    <div class="flex flex-col items-center">
                                         <label for="barcode_id"
-                                        class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Barcode ID:</label>
-                                            <div class="h-16">
-                                        {!! DNS1D::getBarcodeHTML("$products->product_code", 'C128', 2, 60) !!} 
-                                        <p>p - {{ $products->product_code }}</p>
-                                            </div> 
+                                            class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Barcode ID:</label>
+                                        <div class="h-16">
+                                            {!! DNS1D::getBarcodeHTML("$products->product_code", 'C128', 2, 60) !!}
+                                            <p>p - {{ $products->product_code }}</p>
                                         </div>
-    
+                                        <br>
+                                        {{-- Download button --}}
+                                        <a href="{{ route('user.product-information.downloadBarcode', $products->product_code) }}"
+                                            class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                                            download>
+                                            Download Barcode
+                                        </a>
+                                    </div>
+
                                     <div class="md:hidden">
                                         <button type="submit"
-                                            class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center" disabled>
+                                            class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                                            disabled>
                                             Update
                                         </button>
                                         <div class="md:hidden absolute mr-[93px]">
@@ -610,6 +611,31 @@
                 itemToEdit: null, // Variable to store the selected item
             }));
         });
+    </script>
+
+    <script>
+        function generateAndDownloadBarcode() {
+            var productCode = "{{ $products->product_code }}"; // Get product code
+            var barcodeHtml = `{!! DNS1D::getBarcodeHTML("$products->product_code", 'C128', 2, 60) !!}`; // Generate barcode HTML
+
+            // Create a temporary div element to hold the barcode HTML
+            var tempDiv = document.createElement('div');
+            tempDiv.innerHTML = barcodeHtml;
+
+            // Convert barcode SVG to base64 encoded PNG image
+            var barcodeSvg = tempDiv.firstChild;
+            var svgString = new XMLSerializer().serializeToString(barcodeSvg);
+            var image = new Image();
+            image.src = 'data:image/svg+xml;base64,' + btoa(svgString);
+
+            // Create a temporary anchor element to trigger the download
+            var a = document.createElement('a');
+            a.href = image.src;
+            a.download = 'barcode.png'; // Set the filename for the downloaded image
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
     </script>
     @endif
 </x-app-layout>
